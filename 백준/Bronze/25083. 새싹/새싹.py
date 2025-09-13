d5d0d06284dabc45e919e9a868f3a6b13cfd4c17
Chaeyoon Han <113782929+chaeyh4@users.noreply.github.com>
2025-09-13 14:27:48 +0900

    [Bronze V] Title: 새싹, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,12 @@
+line_1 = '         ,r\'\"7'
+line_2 = 'r`-_   ,\'  ,/'
+line_3 = ' \. ". L_r\''
+line_4 = '   `~\/'
+line_5 = '      |'
+line_6 = '      |'
+print(line_1)
+print(line_2)
+print(line_3)
+print(line_4)
+print(line_5)
+print(line_6)
