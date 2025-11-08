170bbef7fff7f3123cd5e419e9a1352f7daba0e9
Chaeyoon Han <113782929+chaeyh4@users.noreply.github.com>
2025-11-08 23:27:27 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
+    
+
+
+if __name__ == "__main__":
+    N = int(input())
+    print(factorial(N))
