8c1b88564f02aa84d88651df24e92026f4373118
Chaeyoon Han <113782929+chaeyh4@users.noreply.github.com>
2025-11-20 10:13:27 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,13 @@
+import sys
+
+if __name__ == "__main__":
+    input = sys.stdin.readline
+    n = int(input())
+    string_list = list(input().rstrip())
+    num_list = []
+    for l in string_list:
+        num_list.append(ord(l)-96)
+    hash = 0
+    for i, n in enumerate(num_list):
+        hash += n * (31**i)
+    print(hash)
