7bf3a62725a07ec93f6f6614d4efe78068dabadd
Chaeyoon Han <113782929+chaeyh4@users.noreply.github.com>
2025-12-04 10:47:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+people = list(range(1, n + 1))
+result = []
+
+idx = 0  
+
+while people:
+    idx = (idx + k - 1) % len(people)
+    result.append(people.pop(idx))
+
+print("<" + ", ".join(map(str, result)) + ">")
