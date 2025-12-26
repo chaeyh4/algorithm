6b573847c7b8a673dc3fb8da741c91c8815466a8
Chaeyoon Han <113782929+chaeyh4@users.noreply.github.com>
2025-12-26 17:42:37 +0900

    [Bronze V] Title: Lucky 7, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    num = input()
+    num_list = list(num)
+    if "7"  in num_list:
+        if int(num) % 7 == 0:
+            print(3)
+        else:
+            print(2)
+    else:
+        if int(num) % 7 == 0:
+            print(1)
+        else:
+            print(0)
