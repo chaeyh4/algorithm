f0761a8e51edefc498fcd6129b0c5d6455258dec
Chaeyoon Han <113782929+chaeyh4@users.noreply.github.com>
2026-01-13 18:15:29 +0900

    [Silver III] Title: 1로 만들기, Time: 448 ms, Memory: 40224 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    x = int(input())
+
+    dp = [0] * (x + 1)
+    dp[1] = 0
+
+    for i in range(2, x + 1):
+        dp[i] = dp[i-1] + 1
+
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i//2] + 1)
+
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i//3] + 1)
+
+    print(dp[x])
