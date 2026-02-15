65693df939dda195cb922dc236c0edfeda343c78
Chaeyoon Han <113782929+chaeyh4@users.noreply.github.com>
2026-02-16 06:44:52 +0900

    [Bronze II] Title: 피보나치 수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+if __name__ == "__main__":
+    n = int(input().rstrip())
+    f = [0]*46
+    f[0] = 0
+    f[1] = 1
+    for i in range(2,46):
+        f[i] = f[i-1]+f[i-2]
+    print(f[n])
